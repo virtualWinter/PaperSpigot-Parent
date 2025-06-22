pvginkel/Tweakkit-Server

Add Optional Tick Shuffling
This prevents players from 'gaming' the server, and strategically relogging to increase their position in the tick order.

src/main/java/net/minecraft/server/ServerConnection.java
@@ -53,6 +53,13 @@ public class ServerConnection {
         List list = this.f;
 
         synchronized (this.f) {
+            // Spigot Start
+            // This prevents players from 'gaming' the server, and strategically relogging to increase their position in the tick order
+            if ( org.spigotmc.SpigotConfig.playerShuffle > 0 && MinecraftServer.currentTick % org.spigotmc.SpigotConfig.playerShuffle == 0 )
+            {
+                Collections.shuffle( this.f );
+            }
+            // Spigot End
             Iterator iterator = this.f.iterator();
 
             while (iterator.hasNext()) {

virtualWinter/PaperSpigot-Parent

PaperSpigot-Server/src/main/java/net/minecraft/server/ServerConnection.java
@@ -123,6 +123,13 @@ public class ServerConnection {
         List list = this.h;
 
         synchronized (this.h) {
+            // Spigot Start
+            // This prevents players from 'gaming' the server, and strategically relogging to increase their position in the tick order
+            if ( org.spigotmc.SpigotConfig.playerShuffle > 0 && MinecraftServer.currentTick % org.spigotmc.SpigotConfig.playerShuffle == 0 )
+            {
+                Collections.shuffle( this.h );
+            }
+            // Spigot End
             Iterator iterator = this.h.iterator();
 
             while (iterator.hasNext()) {